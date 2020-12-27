afd9915ac79053f70c1c310efcc0ca170bc6088d
Robert Medina <robert@fitco.com.pe>
2020-12-27 01:31:46 -0500

    Project structure
    This project will be developing with NestJS as a framework to make commiteable's API Rest

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger: Logger = new Logger('bootstrap');
+  const port: number = 3000;
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
+}
+bootstrap();
